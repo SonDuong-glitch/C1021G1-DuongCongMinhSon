46af20949f4da393bb9450c3d5904a59c8159adb
Duong Son <you@example.com>
2021-12-07 08:06:59 +0700

    push bài tập 07/12/2021

---
+++ Module_2/src/week1_class_obj/week1_class_obj_thuchanh/Main.java
@@ -0,0 +1,17 @@
+package week1_class_obj.week1_class_obj_thuchanh;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input height: ");
+        double height = scanner.nextDouble();
+        System.out.println("Input width : ");
+        double width = scanner.nextDouble();
+        Rectangle rectangle = new Rectangle(height,width);
+        System.out.println("Your Area: "+rectangle.getArea());
+        System.out.println("Your Perimeter: "+rectangle.getPerimeter());
+        System.out.println(rectangle.display());
+    }
+}
---
+++ Module_2/src/week1_class_obj/week1_class_obj_thuchanh/Rectangle.java
@@ -0,0 +1,39 @@
+package week1_class_obj.week1_class_obj_thuchanh;
+
+public class Rectangle {
+
+    private double height, width;
+
+    public Rectangle(){
+
+    }
+    public Rectangle(double height,double width){
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getArea(){
+        return this.height*this.width;
+    }
+    public double getPerimeter(){
+        return (this.height + this.width)*2;
+    }
+    public String display(){
+        return "Retangle {"+" width: "+width+" height: "+height+" } ";
+    }
+}
