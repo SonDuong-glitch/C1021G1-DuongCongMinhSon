a3876ffd20692658f3a082ecf29df97a4117f0b1
SonDuong-glitch <83948475+SonDuong-glitch@users.noreply.github.com>
2021-12-17 15:25:05 +0700

    update bài tập 17/12/2021

---
+++ Module_2/src/week3/baitap/Search.java
@@ -0,0 +1,31 @@
+package week3.baitap;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập chuỗi");
+        String input = sc.nextLine();
+        LinkedList<Character> max = new LinkedList<>();
+        for ( int i = 0 ; i < input.length();i++){
+            LinkedList<Character> list = new LinkedList<>();
+            list.add(input.charAt(i));
+        for(int j = i +1; j < input.length() ; j++ ){
+            if(input.charAt(i) > list.getLast()){
+                list.add(input.charAt(j));
+            }
+        }
+        if(list.size() > max.size()){
+            max.clear();
+            max.addAll(list);
+        }
+        list.clear();
+        }
+        for (Character ch: max) {
+            System.out.print(ch);
+        }
+        System.out.println();
+    }
+}
