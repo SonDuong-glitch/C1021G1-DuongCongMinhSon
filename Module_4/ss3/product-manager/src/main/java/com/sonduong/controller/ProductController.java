21ddc1deb98c7fca8916403fcf8c425cceb1a541
SonDuong-glitch <83948475+SonDuong-glitch@users.noreply.github.com>
2022-03-04 15:30:15 +0700

    push bai tap

--- Module_4/ss3/product-manager/src/main/java/com/sonduong/controller/ProductController.java
+++ Module_4/ss3/product-manager/src/main/java/com/sonduong/controller/ProductController.java
@@ -6,13 +6,15 @@ import com.sonduong.service.ProductImp;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("")
 public class ProductController {
     private static IProduct product = new ProductImp();
     @GetMapping("")
@@ -26,5 +28,30 @@ public class ProductController {
         model.addAttribute("product", new Product());
         return "create";
     }
-
+    @PostMapping("/save")
+    public String save(Product product){
+        product.setId((int) (Math.random() * 10000));
+        return "redirect:/";
+    }
+    @GetMapping("/product/{id}/edit")
+    public String edit(@PathVariable  int id, Model model) {
+        model.addAttribute("product", product.findById(id));
+        return "edit";
+    }
+    @GetMapping("/product/{id}/delete")
+    public String delete(Product product1, RedirectAttributes redirect) {
+        product.delete(product1.getId());
+        redirect.addAttribute("success", "Removed customer successfully!");
+        return "redirect:/";
+    }
+    @PostMapping("/update")
+    public String update(Product product1) {
+        product.update(product1.getId(), product1);
+        return "redirect:/";
+    }
+    @GetMapping("product/{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("product", product.findById(id));
+        return "infor";
+    }
 }
