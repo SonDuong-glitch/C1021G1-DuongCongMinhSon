ef018bb98a6964b5a64825ca2f5c72585c4683de
SonDuong-glitch <83948475+SonDuong-glitch@users.noreply.github.com>
2022-03-10 14:25:35 +0700

    push bai tap

--- Module_4/ss6/blog/src/main/java/com/example/blog/controller/BlogControler.java
+++ Module_4/ss6/blog/src/main/java/com/example/blog/controller/BlogControler.java
@@ -1,7 +1,7 @@
 package com.example.blog.controller;
 
 import com.example.blog.model.Blog;
-import com.example.blog.service.IBlog;
+import com.example.blog.service.BlogImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +9,11 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.List;
-
 @Controller
 public class BlogControler {
 @Autowired
-    private IBlog iBlog;
+    private BlogImpl iBlog;
 @GetMapping("/create")
     public ModelAndView showCreateForm(){
     ModelAndView modelAndView = new ModelAndView("/blog/create");
@@ -41,11 +39,49 @@ public class BlogControler {
     public ModelAndView showEditForm(@PathVariable int id){
     Blog blog = iBlog.findById(id);
     if(blog != null){
-        ModelAndView modelAndView = new ModelAndView("/blog/edit");
+        ModelAndView modelAndView = new ModelAndView("blog/edit");
+        modelAndView.addObject("blog", blog);
         return modelAndView;
     }else{
-        ModelAndView modelAndView = new ModelAndView("/blog/error404");
+        ModelAndView modelAndView = new ModelAndView("blog/error404");
         return modelAndView;
     }
 }
+@PostMapping("/edit")
+    public ModelAndView updateBlog(@ModelAttribute("blog") Blog blog){
+    iBlog.save(blog);
+    ModelAndView modelAndView = new ModelAndView("/blog/edit");
+    modelAndView.addObject("blog",blog);
+    modelAndView.addObject("message", "Update success");
+    return modelAndView;
+}
+@GetMapping("remove/{id}")
+    public  ModelAndView deleteShowForm(@PathVariable int id ){
+        Blog blog = iBlog.findById(id);
+        if(blog != null){
+            ModelAndView modelAndView = new ModelAndView("blog/remove");
+            modelAndView.addObject("blog" , blog);
+            return modelAndView;
+        }else {
+            ModelAndView modelAndView = new ModelAndView("blog/error404");
+            return modelAndView;
+        }
     }
+    @PostMapping("remove")
+    public String deleteBlog(@ModelAttribute("blog") Blog blog){
+        iBlog.remove(blog.getId());
+        return "redirect:/";
+    }
+    @GetMapping("viewOne/{id}")
+    public ModelAndView findOne(@PathVariable int id){
+        Blog blog = iBlog.findById(id);
+        if(blog != null){
+            ModelAndView modelAndView = new ModelAndView("blog/viewOne");
+            modelAndView.addObject("blog",blog);
+            return modelAndView;
+        }else{
+            ModelAndView modelAndView = new ModelAndView("blog/error404");
+            return modelAndView;
+        }
+    }
+}
--- Module_4/ss6/blog/src/main/java/com/example/blog/service/BlogImpl.java
+++ Module_4/ss6/blog/src/main/java/com/example/blog/service/BlogImpl.java
@@ -3,9 +3,9 @@ package com.example.blog.service;
 import com.example.blog.model.Blog;
 import com.example.blog.reponsitory.IBlogReponsitory;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
-
+@Service
 public class BlogImpl implements IBlog{
     @Autowired
     private IBlogReponsitory iBlogReponsitory;
