15e0b96873a333a69d2d6cd8be8128c914cf098c
SonDuong-glitch <83948475+SonDuong-glitch@users.noreply.github.com>
2022-03-20 20:36:14 +0700

    push bai tap

---
+++ Module_4/ss10/them_san_pham_vao_gio_hang/src/main/java/com/example/them_san_pham_vao_gio_hang/model/Cart.java
@@ -0,0 +1,28 @@
+package com.example.them_san_pham_vao_gio_hang.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+    private Map<Product,Integer> products = new HashMap<>();
+
+    public Cart() {
+    }
+
+    public Cart(Map<Product,Integer> products) {
+        this.products = products;
+    }
+
+    public Map<Product,Integer> getProducts() {
+        return products;
+    }
+    private boolean checkItemInCart(Product product){
+        for (Map.Entry<Product, Integer> entry : products.entrySet()) {
+            if(entry.getKey().getId().equals(product.getId())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
